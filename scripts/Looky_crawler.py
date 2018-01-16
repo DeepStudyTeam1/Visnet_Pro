c3e8b693f2a92f98607c23fb6a5f375b9851e019
lllldoodoo <cksdnd619@naver.com>
2018-01-17 02:22:17 +0900

    .

--- scripts/Looky_crawler.py
+++ scripts/Looky_crawler.py
@@ -3,23 +3,46 @@ import urllib.request
 
 OUTPUT_FILE_NAME = 'output.pkl'
 
-URL = 'http://www.afit.co.kr/m/product.html?branduid=86961&xcode=001&mcode=001&scode=&type=X&sort=order&cur_code=001001&GfDT=bW93U10%3D'
+URL = 'http://www.bymono.com/product/detail.html?product_no=22953&cate_no=194&display_group=1'
 
 
 # 크롤링 함수
-def get_text (URL, shop):
+def get_image_url (URL, shop):
+    source_code_from_URL = urllib.request.urlopen (URL)
+    soup = BeautifulSoup (source_code_from_URL, 'lxml', from_encoding='utf-8')
     if shop == 'afit' :
-        source_code_from_URL = urllib.request.urlopen (URL)
-        soup = BeautifulSoup (source_code_from_URL, 'lxml', from_encoding='utf-8')
-        for link in soup.find_all ('a'):
-            print (link.get ('href'))
+        for something in soup.find_all ('link'):
+            some= something.get("rel")
+            if some == ["image_src"]:
+                print(something.get('href'))
+    elif shop == 'allthumb':
+        for something in soup.find_all ('img'):
+            some = something.get("class")
+            print(some)
+            if some == ["bigImage"]:
+                print(something.get('src'))
+    elif shop == 'bechic':
+        for something in soup.find_all ('img'):
+            some = something.get("class")
+            if some == ["BigImage", '']:
+                print(something.get('src'))
+    elif shop == 'blueforce':
+        for something in soup.find_all ('li'):
+            some = something.get("class")
+            if some == ['origin-img']:
+                print("http://www.blueforce.co.kr" + something.a.img.get('src'))
+    elif shop == 'bymono':
+        for something in soup.find_all ('div'):
+            some = something.get("class")
+            if some == ['xans-element-', 'xans-product', 'xans-product-image', 'imgArea', '']:
+                print("http:" + something.img.get('src'))
 
     return
 
 
 # 메인 함수
 def main ():
-    get_text (URL, 'afit')
+    get_image_url (URL, 'bymono')
 
 
 if __name__ == '__main__':
