72e360fe1ba2b9ebcc9b77dfc025b67afd6ea687
lllldoodoo <cksdnd619@naver.com>
2018-01-16 22:46:42 +0900

    .

--- model/Test.py
+++ model/Test.py
@@ -34,9 +34,9 @@ def show_image(predictions):
         path = base_dir + "/images/" + str(i) + ".jpg"
         Image.open(path).show()
 
-def test(img_id, vertical):
+def test(img_id, vertical, topk = 10):
 
-    #show_image([img_id])
+    show_image([img_id])
 
     img_path = base_dir + "/images/"+ str(img_id) + ".jpg"
 
@@ -46,30 +46,29 @@ def test(img_id, vertical):
 
     file_path = base_dir + "/feature/" + vertical + ".pkl"
     feature_all = torch.load(file_path)
-    feature_all -= feature
+    feature_all = feature_all - feature.data
     feature_all = feature_all.norm(2,1)
-    
-
-
+    _ , top_index = torch.topk(feature_all, topk , largest= False)
+    with open(base_dir + "/image_lists/" + vertical + "_retrieval.pkl", 'rb') as f:
+        line = pickle.load(f)
+    top_id = []
+    for index in top_index:
+        top_id.append(line[index])
 
     print("show predict")
-    predict = knn.kneighbors(feature, return_distance=False)
-    show_image(predict)
+    print(top_id)
 
-
-
-
-
+    show_image(top_id)
 
     with open(base_dir + "/image_lists/" + vertical + "_test.pkl", 'rb') as f:
         q_to_p_map = pickle.load(f)
 
     count = 0
 
-    for i in predict:
+    for i in top_id:
         if i in q_to_p_map[img_id]:
             count += 1.0
 
     print("Acc : %.3f" %(count / len(q_to_p_map)))
 
-test(9600,"tops")
+test(2337,"tops")
